09acf229955dff9ed9340cb156870a8445af6f45
Abhishek Sharma <abhi.sharma2800015@gmail.com>
2023-11-23 00:57:55 +0530

    Q queries problem solved

--- src/GearingUp(M2)/ArraysAndDynamicArrays/PreComputation.js
+++ src/GearingUp(M2)/ArraysAndDynamicArrays/PreComputation.js
@@ -16,6 +16,17 @@ async function preComputation() {
     numberArray.push(Number(element.trim()));
   }
   prefixSum(numberArray);
+  const numberOfQueries = await askQuestion("Enter the number of queries : ");
+  let l, r;
+  for (let i = 0; i < Number(numberOfQueries.trim()); i++) {
+    l = await askQuestion("Enter the value of l : ");
+    r = await askQuestion("Enter the value of r : ");
+    if (Number(l.trim()) < 0 || Number(r.trim()) > numberArray.length - 1) {
+      console.log("Invalid values");
+    } else {
+      console.log(numberArray[r] - numberArray[l - 1]);
+    }
+  }
   console.log(numberArray);
 }
 
